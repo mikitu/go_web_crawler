d06d1296679c8f5332faef2f2efe392906eaf001
Mihai BUCSE <mikitu78@gmail.com>
2017-04-04 20:11:11 +0100

    check for http status

--- src/fetcher/default_http_fetcher.go
+++ src/fetcher/default_http_fetcher.go
@@ -3,6 +3,8 @@ package fetcher
 import (
 	"net/http"
 	"bytes"
+	log "github.com/sirupsen/logrus"
+	"errors"
 )
 
 type DefaultHttpFetcher struct {}
@@ -11,11 +13,19 @@ func NewDefaultHttpFetcher() * DefaultHttpFetcher {
 }
 
 func (f DefaultHttpFetcher) Fetch(url string) (*bytes.Buffer, error) {
+	// TODO: provide a way to delay the requests after a number
+	// of consecutive requests. some websites responds with
+	// status 429 Too many requests
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Error("Something is wrong with the remote server")
+		log.Errorf("StatusCode:  %d\n", resp.StatusCode )
+		return nil, errors.New("Error: StatusCode is  " + resp.Status)
+	}
 	buf := new(bytes.Buffer)
 	_, err = buf.ReadFrom(resp.Body)
 
